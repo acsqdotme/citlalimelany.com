3ffd9b6ac353beb6c6ffe48d660cac8e9773473d
Ángel Castañeda <angelaxel1420@disroot.org>
2023-08-31 17:38:08 -0500

    Add template engine
    I keep repeating the helper funcs doesFileExist and bindTemplate.

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,17 +1,70 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
+	"strings"
 )
 
-func pageHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		r.URL.Path = "/"
+const (
+	htmlDir     = "./html"
+	tmplFileExt = ".tmpl.html"
+)
+
+func doesFileExist(pathToFile string) bool {
+	info, err := os.Stat(filepath.Clean(pathToFile))
+	if err != nil || info.IsDir() {
+		return false
 	}
-	http.ServeFile(w, r, filepath.Join("./html", r.URL.Path))
+	return true
+}
+
+func bindTMPL(files ...string) (*template.Template, error) {
+	for _, checkFile := range files {
+		if !doesFileExist(checkFile) {
+			return nil, errors.New("Template file missing " + checkFile)
+		}
+	}
+
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		return nil, err
+	}
+
+	return tmpl, nil
+}
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	path := strings.Split(r.URL.Path, "/")
+	page := path[1]
+
+	if r.URL.Path == "/" {
+		page = "index"
+	}
+
+	if !doesFileExist(filepath.Join(htmlDir, "pages", page+tmplFileExt)) {
+		http.Error(w, "page not found", 404)
+		return
+	}
+
+	tmpl, err := bindTMPL(
+		filepath.Join(htmlDir, "base"+tmplFileExt),
+		filepath.Join(htmlDir, "pages", page+tmplFileExt),
+	)
+	if err != nil {
+		http.Error(w, "template broke", 500)
+		log.Println(err.Error())
+		return
+	}
+
+	tmpl.ExecuteTemplate(w, "base", nil)
 }
 
 func main() {
