6df70469853847151c9207f6df23c4687211cdf7
Ángel Castañeda <angel@acsq.me>
2023-09-30 23:55:53 -0500

    Add https flag for redirects

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -8,12 +8,22 @@ import (
 	"citlalimelany.com/album"
 )
 
+var (
+	scheme = "http"
+)
+
 func main() {
 	if err := album.MakeDB(); err != nil {
 		log.Fatal(err)
 	}
+
 	addr := flag.String("addr", ":4001", "HTTP Server Port Address")
+	https := flag.Bool("https", false, "Enable HTTPS for Deployment")
 	flag.Parse()
+
+	if *https {
+		scheme = "https"
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", pageHandler)
